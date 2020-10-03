5a344dd443cba914bc8fe0f4a4e7e505855d01b2
alimardani94 <alimardani94@gmail.com>
2020-10-03 13:22:01 +0330

    use milad php-jwt service

---
+++ app/Services/Token/Jwt.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services\Token;
+
+use Exception;
+use MiladRahimi\Jwt\Cryptography\Algorithms\Hmac\HS512;
+use MiladRahimi\Jwt\Cryptography\Signer;
+use MiladRahimi\Jwt\Cryptography\Verifier;
+use MiladRahimi\Jwt\Generator;
+use MiladRahimi\Jwt\Parser;
+
+class Jwt implements Token
+{
+    /**
+     * @var Signer
+     */
+    private $signer;
+
+    /**
+     * @var Verifier
+     */
+    private $verifier;
+
+    /**
+     * Jwt constructor.
+     *
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $this->signer = $this->verifier = new HS512(config('token.types.jwt.key'));
+    }
+
+    /**
+     * @inheritDoc
+     * @throws Exception
+     */
+    public function generate(int $userId): string
+    {
+        return (new Generator($this->signer))->generate([
+            'sub' => $userId,
+            'exp' => time() + config('token.types.jwt.ttl'),
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function extract(string $jwt): ?int
+    {
+        try {
+            return (new Parser($this->verifier))->parse($jwt)['sub'];
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+}
---
+++ app/Services/Token/Token.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Token;
+
+interface Token
+{
+    /**
+     * Generate token from user
+     *
+     * @param int $userId
+     * @return string
+     */
+    public function generate(int $userId): string;
+
+    /**
+     * Extract user id form token
+     *
+     * @param string $jwt
+     * @return int|null
+     */
+    public function extract(string $jwt): ?int;
+}
