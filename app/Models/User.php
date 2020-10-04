b7a1dc114989f7dc94fd902248196b204beff957
alimardani94 <alimardani94@gmail.com>
2020-10-04 10:30:28 +0330

    add auth.admin middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -90,4 +90,12 @@ class User extends Authenticatable
     {
         return isset($this->first_name) ? $this->first_name . ' ' . $this->last_name : $this->cellphone;
     }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return false;
+    }
 }
