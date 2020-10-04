e93c7b4f0d58f1eaded2e7c4fd7da77148e0e96f
alimardani94 <alimardani94@gmail.com>
2020-10-04 10:52:56 +0330

    add persian lang files

---
+++ resources/lang/fa/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'شناسه یا گذرواژه شما نادرست می باشد.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'otp-sms' => "کد ورود به کنداکتور \nCode: :otp",
+    'code_sent' => 'کد ورود برای شما پیامک شد.',
+    'account_suspended' => 'حساب شما مسدود شده است لطفا با پشتیبانی تماس بگیرید',
+    'account_suspended_for_days' => '{1} حساب شما برای یک روز مسدود شده است لطفا با پشتیبانی تماس بگیرید|[2,*] حساب شما برای :day روز مسدود شده است لطفا با پشتیبانی تماس بگیرید',
+    'account_suspended_for_minutes' => '{1} حساب شما برای یک دقیقه مسدود شده است|[2,*] حساب شما برای :minute دقیقه مسدود شده است',
+];
